1463b88aca03dfd138ab14c2e07cf10c02c90c00
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-10-28 14:50:13 -0300

    First commit

---
+++ src/main/java/emailsender/EmailSender.java
@@ -0,0 +1,16 @@
+package emailsender;
+
+public class EmailSender {
+	
+	public static void main(String[] args) {
+		final String fromEmail = "example@gmail.com";		
+		final String password = "******";
+		final String toEmail = "example@gmail.com";
+		
+		System.out.println("Initializing email send");
+		
+		EmailConfig config = new EmailConfig();
+		
+		config.sendEmail(fromEmail, password, toEmail, "Tutorial JavaMail API", "Hello World!");
+	}
+}
